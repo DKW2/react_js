f6252d12b3f97bb62b95e86b0362af6c8c16b4cf
Derek Wang <derek.kiah.wang@gmail.com>
2025-10-08 01:54:31 -0700

    Utilized cursor to generate pretty decent CSS/HTML structure for the components

--- client/src/App.js
+++ client/src/App.js
@@ -14,20 +14,56 @@ function App() {
   }
 
   return (
-    <div>
-      <h1>Funny Project</h1>
-      <GrowingText />
-      <ColorfulText />
-      <ChangingText textProperty='fontSize' textChange={textChange} />
-      <Predict/>
-      <RandomNumber />
-      <div classname='Boo'>
-        {Array.from( {length: 3}, (_, i) => (
-          <TimedJumpscare key={i} waitTime={(i + 1) * 1000} scareTime={(i + 1) * 1000} />
-        )) }
-        {/* <TimedJumpscare waitTime={1000} scareTime={1000}/>
-        <TimedJumpscare waitTime={1000} scareTime={1000}/>
-        <TimedJumpscare waitTime={1500} scareTime={1000}/> */}
+    <div className="page">
+      <div className="container">
+        <h1 className="title">Funny Project</h1>
+        <div className="grid">
+          <section className="card half">
+            <h2>Growing Text</h2>
+            <div className="card-content">
+              <GrowingText />
+            </div>
+          </section>
+
+          <section className="card half">
+            <h2>Colorful Text</h2>
+            <div className="card-content">
+              <ColorfulText />
+            </div>
+          </section>
+
+          <section className="card half">
+            <h2>Changing Text</h2>
+            <div className="card-content">
+              <ChangingText textProperty='fontSize' textChange={textChange} />
+            </div>
+          </section>
+
+          <section className="card half">
+            <h2>Predict</h2>
+            <div className="card-content">
+              <Predict/>
+            </div>
+          </section>
+
+          <section className="card half">
+            <h2>Random Number</h2>
+            <div className="card-content">
+              <RandomNumber />
+            </div>
+          </section>
+
+          <section className="card full">
+            <h2>Timed Jumpscares</h2>
+            <div className="card-content">
+              <div className='Boo'>
+                {Array.from( {length: 3}, (_, i) => (
+                  <TimedJumpscare key={i} waitTime={(i + 1) * 1000} scareTime={(i + 1) * 1000} />
+                )) }
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
     </div>
   );
